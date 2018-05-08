07ef65eaed0f6781c1cbd895e0aec7b39ae1be89
ALI ASM IFTEKHAR <ali.iftekhar343@gmail.com>
2018-05-08 23:53:09 +0600

    Add project files.

---
+++ CustomTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethodExample;
+
+
+namespace CustomTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtensionMethodCustom onj = new ExtensionMethodCustom();
+        }
+    }
+    public static class xx
+    {
+        public static void Test3(this ExtensionMethodCustom obj)
+        {
+            Console.WriteLine("i am test3");
+        }
+    }
+}
---
+++ ExtensionMethodExample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "dot net perls";
+            value = value.UppercaseFirstLetter();
+            ExtensionMethodsCustom obj = new ExtensionMethodsCustom();
+            obj.Test1();
+            obj.Test2();
+            obj.Test3();
+            int a = 10.MultiplyBy(2);
+            Console.WriteLine(a);
+            Console.ReadKey();
+
+        }
+    }
+
+
+    public static class ExtensionMethods
+    {
+        public static string UppercaseFirstLetter(this string value)
+        {
+            // Uppercase the first letter in the string.
+            if (value.Length > 0)
+            {
+                char[] array = value.ToCharArray();
+                array[0] = char.ToUpper(array[0]);
+                return new string(array);
+            }
+            return value;
+        }
+        public static int MultiplyBy(this int value, int mulitiplier)
+        {
+            // Uses a second parameter after the instance parameter.
+            return value * mulitiplier;
+        }
+    }
+
+    public  class ExtensionMethodsCustom
+    {
+        public  void Test1()
+        {
+            Console.WriteLine("I am test 1");
+        }
+        public  void Test2()
+        {
+            Console.WriteLine("I am test 2");
+        }
+    }
+    public static class CustomeExtensionMethodAddNewFunction
+    {
+        public static void Test3(this ExtensionMethodsCustom obj)
+        {
+            Console.WriteLine("i am test3");
+        }
+    }
+
+}
