a414d1382357aa6397920420a4e95baab68ba3ce
ALI ASM IFTEKHAR <ali.iftekhar343@gmail.com>
2018-05-08 23:55:25 +0600

    ExtionMethod Full

--- ExtensionMethodExample/Program.cs
+++ ExtensionMethodExample/Program.cs
@@ -6,24 +6,15 @@ using System.Threading.Tasks;
 
 namespace ExtensionMethodExample
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string value = "dot net perls";
-            value = value.UppercaseFirstLetter();
-            ExtensionMethodsCustom obj = new ExtensionMethodsCustom();
-            obj.Test1();
-            obj.Test2();
-            obj.Test3();
-            int a = 10.MultiplyBy(2);
-            Console.WriteLine(a);
-            Console.ReadKey();
 
-        }
-    }
+         /*
+    
+           An extension method is a static method of a static class, 
+           where the "this" modifier is applied to the first parameter. 
+           The type of the first parameter will be the type that is extended.
+           Extension methods are only in scope when you explicitly import the namespace into your source code with a using directive.
 
-
+        */
     public static class ExtensionMethods
     {
         public static string UppercaseFirstLetter(this string value)
@@ -44,13 +35,13 @@ namespace ExtensionMethodExample
         }
     }
 
-    public  class ExtensionMethodsCustom
+    public class ExtensionMethodsCustom
     {
-        public  void Test1()
+        public void Test1()
         {
             Console.WriteLine("I am test 1");
         }
-        public  void Test2()
+        public void Test2()
         {
             Console.WriteLine("I am test 2");
         }
@@ -60,6 +51,23 @@ namespace ExtensionMethodExample
         public static void Test3(this ExtensionMethodsCustom obj)
         {
             Console.WriteLine("i am test3");
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string value = "dot net perls";
+            value = value.UppercaseFirstLetter();
+            ExtensionMethodsCustom obj = new ExtensionMethodsCustom();
+            obj.Test1();
+            obj.Test2();
+            obj.Test3();
+            int a = 10.MultiplyBy(2);
+            Console.WriteLine(a);
+            Console.ReadKey();
+
         }
     }
 
